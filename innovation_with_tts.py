def00e0deb7f53f5471ad2480acc1ef820cac88a
Arjun <arjungravi1@gmail.com>
2024-02-06 19:54:03 +0530

    Added new TTS

--- innovation_with_tts.py
+++ innovation_with_tts.py
@@ -7,7 +7,7 @@ import whisper
 import audioop  
 
 # Parameters to calibrate
-silence_thresh = 700 # Adjusts the volume level to be considered 'silent'.
+silence_thresh = 150 # Adjusts the volume level to be considered 'silent'.
 max_duration = 60 # Max recording duration, regardless of everything else.
 max_silence_seconds = 2 # How much silence duration is to be considered 'done talking'
 model_name = 'small.en' # Whisper model
